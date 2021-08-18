0d47b2812ba0a3975e8885c927506ae90ed5aa1f
Felippe Mac <86482076+lippao@users.noreply.github.com>
2021-08-18 11:48:45 -0300

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,50 @@
+print('hello world')
+print('Hello + World')
+
+nota = 8,5
+disciplina = 'History'
+
+print(nota)
+print(disciplina)
+
+preco = 9,90
+print(preco)
+
+a = 1 #a recebe 1
+b = 5 #b recebe 5
+
+resposta = a == b
+print(resposta)
+
+resposta = a != b
+print(resposta)
+f
+frase = 'ola mundo'
+print(frase[0])
+print(frase[5])
+
+s1 = 'Youtube'
+s1 = s1 +' & Facebook'
+print(s1)
+
+s1 = 'Jesus esta com voce' + ' sempre' * 555 + ' saca?'
+print(s1)
+
+nota = 8.5
+s1 = 'Voce tirou %f na disciplina de History' % nota
+print(s1)
+
+s1 = 'Voce tirou %.2f na disciplina de History' % nota
+print(s1)
+
+nota = 8.5
+disciplina = 'History'
+
+s1 = 'Voce tirou {} na disciplina de {}'.format(nota, disciplina)
+print(s1)
+print('>')
+
+s1 = 'Logica de programaçao e algoritmos'
+tamanho = len(s1)
+print(tamanho)
+
